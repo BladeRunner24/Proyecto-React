ebcfe14e1edfab844b99a4ce761d8d2742edd0f1
Carlos Arturo Mihanovich <carlosmihanovich@hotmail.com>
2020-10-24 22:32:20 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import NavBar from './components/NavBar.js';
+import ItemListContainer from './components/itemListContainer.js';
 import './App.css';
 
 function App() {
@@ -8,19 +8,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <NavBar/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+       <ItemListContainer/>
     </div>
   );
 }
