407b27678e4555a460923d5cdab32b4e1d3f28d7
Carlos Arturo Mihanovich <carlosmihanovich@hotmail.com>
2020-11-07 20:37:35 -0300

    Update ItemList.js

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -4,6 +4,6 @@ import Item from "./item.js";
 export default function ListaItems(props) {
   const empleados = props.listado;
   return empleados.map((item) => (
-    <Item id={item.id} titulo={item.title} precio={item.price} imagen={item.image} />
+    <Item id={item.id} title={item.title} price={item.price} image={item.image} />
   ));
 }
