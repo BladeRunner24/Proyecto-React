9c912324be1b96320898f1d5fc7c7dcfea82bccf
Carlos Arturo Mihanovich <carlosmihanovich@hotmail.com>
2020-11-09 15:29:16 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route, Link, useParams} from 'react-router-dom';
 import NavBar from './components/NavBar.js';
 import ItemListContainer from './components/itemListContainer.js';
 import ItemCount from './components/itemCount.js';
 import ItemDetailContainer from './components/itemDetailContainer.js';
 import './App.css';
 
+
+
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <header className="App-header">
         <NavBar/>
       </header>
-       <ItemListContainer title = 'E-COMMERCE'/>
-       <ItemCount title = 'Bicicletas' accion = 'comprar' max =  {5} min = {1} amount = {1}/>
-       <ItemDetailContainer/>
+      <Switch>
+        <Route exact path = "/">
+          <ItemListContainer title = 'E-COMMERCE'/>
+          <ItemCount title = 'Bicicletas' accion = 'comprar' max = {5} min = {1} amount = {1}/>          
+        </Route>
+        <Route exact path = "/otra-ruta/:id">
+            <ItemDetailContainer/>
+        </Route>
+      </Switch> 
     </div>
+    </BrowserRouter>
   );
 }
 
