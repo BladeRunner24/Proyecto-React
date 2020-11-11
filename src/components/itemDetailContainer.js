d7f137f02d105ef5682f08c41266da82db996385
Carlos Arturo Mihanovich <carlosmihanovich@hotmail.com>
2020-11-10 21:00:33 -0300

    Update itemDetailContainer.js

--- src/components/itemDetailContainer.js
+++ src/components/itemDetailContainer.js
@@ -3,7 +3,8 @@ import ItemDetail from "./itemDetail.js";
 
 export default function ItemDetailContainer (props) {
 
-    const [ listadoProds, setListadoProds ] = useState( [ ] );
+    const [ listadoProds, setListadoProds ] = useState([]);
+    
 
   useEffect(() => {
     
@@ -11,11 +12,13 @@ export default function ItemDetailContainer (props) {
       setTimeout(() => {
         res([
           {
+            id: '130b',
             price: 50000,  
             title: 'Mountain Bike Lince',
             description: 'Bicicleta de 6 cambios producida por Lince.',             
           },
           {
+            id: '152a',
             price: 65000,
             title: 'Mountain Bike Tiger',
             description: 'Bicicleta de 8 cambios producida por Tiger. Producida integramente con fibra de carbono',             
@@ -24,7 +27,9 @@ export default function ItemDetailContainer (props) {
       }, 3000);
     });
     list.then(listado => {
-      setListadoProds(listado); 
+      console.log(listado)
+      setListadoProds(listado.filter (l => l.id === '130b')); 
+      console.log(listadoProds)
     }, err => {
        console.log(null);
     }).catch (error => {
@@ -35,6 +40,7 @@ export default function ItemDetailContainer (props) {
   
 
     return  (
-        <ItemDetail listproductos = {listadoProds}/>
+
+        <ItemDetail ids = {listadoProds.id} titulo = {listadoProds.title} descripcion = {listadoProds.description} precio = {listadoProds.price}/>
     )
 };
