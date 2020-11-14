6fc9d337cdb9922f390718689ad562985080cbe1
Carlos Arturo Mihanovich <carlosmihanovich@hotmail.com>
2020-11-14 19:12:39 -0300

    Update itemDetailContainer.js

--- src/components/itemDetailContainer.js
+++ src/components/itemDetailContainer.js
@@ -1,46 +1,40 @@
 import React, {useEffect, useState} from 'react';
 import ItemDetail from "./itemDetail.js";
+import {useParams} from 'react-router-dom';
 
 export default function ItemDetailContainer (props) {
 
-    const [ listadoProds, setListadoProds ] = useState([]);
+    const [ detalleProds, setDetalleProds ] = useState([]);
+    const {id} = useParams();
     
 
   useEffect(() => {
     
     const list = new Promise((res, rej) => {
       setTimeout(() => {
-        res([
+        res(
           {
             id: '130b',
             price: 50000,  
             title: 'Mountain Bike Lince',
             description: 'Bicicleta de 6 cambios producida por Lince.',             
           },
-          {
-            id: '152a',
-            price: 65000,
-            title: 'Mountain Bike Tiger',
-            description: 'Bicicleta de 8 cambios producida por Tiger. Producida integramente con fibra de carbono',             
-          }
-      ]);
+          );
       }, 3000);
     });
     list.then(listado => {
-      console.log(listado)
-      setListadoProds(listado.filter (l => l.id === '130b')[0]); 
-      console.log(listadoProds)
+      setDetalleProds(listado); 
     }, err => {
        console.log(null);
     }).catch (error => {
       console.log(`Se ha producido un error ${error}`);
     })
-  }, []);
+  }, [id]);
 
   
 
     return  (
 
-        <ItemDetail ids = {listadoProds.id} titulo = {listadoProds.title} descripcion = {listadoProds.description} precio = {listadoProds.price}/>
+        <ItemDetail ids = {detalleProds.id} titulo = {detalleProds.title} descripcion = {detalleProds.description} precio = {detalleProds.price}/>
     )
 };
