fd4f12e0c45f7bdc2c54e9a34bdd1f24c268f573
Carlos Arturo Mihanovich <carlosmihanovich@hotmail.com>
2020-11-14 19:10:31 -0300

    Update item.js

--- src/components/item.js
+++ src/components/item.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Item(props) {
 
     const styles = {
        display: 'flex',
        flexDirection: 'column',
+       zIndex: '3',
     }
 
     return (
           <div>
-              <h3>Titulo: {props.title} Product id: {props.id}</h3>
+              <h3>Titulo: {props.title} - Product id: {props.id}</h3>
               <p>Precio: {props.price}</p>
-              <img href = {props.image} alt = 'Mountain Bike'/> 
+              <Link to = {`/item/${props.id}`}><img href = {props.image} alt = 'Mountain Bike'/></Link> 
           </div>
   );
 };
