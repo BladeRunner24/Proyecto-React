e7e1b1e5c5c9cfa0f2fcc50eaad1795eeebe8591
Carlos Arturo Mihanovich <carlosmihanovich@hotmail.com>
2020-11-23 19:56:39 -0300

    Add files via upload

---
+++ src/context/cartContext.js
@@ -0,0 +1,26 @@
+import React, {useContext, useState} from 'react';
+
+export const CartContext = React.createContext([]);
+
+export const UserCartContext = () => useContext(CartContext);
+
+export default function CartProvider({children, defaultcart}) {
+
+    const {cart, setCart} = useState(defaultcart);
+
+    function add(item) {
+        setCart([...cart, item])
+    };
+
+    function remove(itemid) {
+        setCart(cart.filter(item => item.id !== itemid))
+    };
+
+    function clean() {
+        setCart([]);
+    }
+
+    return <CartContext.Provider value = {{cart, add, remove, clean}}>
+        {children}
+    </CartContext.Provider>
+} 
