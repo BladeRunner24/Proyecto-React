0e54a933273dad0478048e617f14f57cfa67ac6b
Carlos Arturo Mihanovich <carlosmihanovich@hotmail.com>
2020-11-24 19:16:29 -0300

    Update itemDetail.js

--- src/components/itemDetail.js
+++ src/components/itemDetail.js
@@ -8,13 +8,21 @@ export default function ItemDetail (props) {
   const [onLine, setOnLine] = useState(true);
   const [guardar, setGuardar] = useState();
   const titulo = 'bicicletas';
-  const {add, remove, clean} = useContext(CartProvider);
+  const {add} = useContext(CartProvider);
+  const [id, setId] = useState();
+   
+  
 
   function onAdd(number){
     setOnLine(false);
     setGuardar(number);
     alert(`Se van a agregar ${number} ${titulo} a su carrito`);
     };
+    
+    setId(props.ids);
+    
+    const item = id + guardar;  
+     
 
   return (
         <>
@@ -25,6 +33,7 @@ export default function ItemDetail (props) {
             <p>{props.precio}</p>             
           </div>
           {onLine ? <ItemCount apagado = {onAdd} title = 'Bicicletas' accion = 'comprar' max = {5} min = {1} amount = {1}/> : <Link to = "/carrito"><button>Terminar mi compra</button></Link>}
+          <button onClick= { add (item) }>Agregar al carrito</button>
         </>
     );
 };
