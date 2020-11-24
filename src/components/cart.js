8e677de51f1929c16926644448b264ddb92cf927
Carlos Arturo Mihanovich <carlosmihanovich@hotmail.com>
2020-11-24 19:17:28 -0300

    Update cart.js

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React,  {useContext} from 'react';
+import  CartProvider from '../context/cartContext.js';
 
 export default function Cart() {
 
+    const {cart, remove, clean} = useContext(CartProvider);
+
+   
     return (
+        <>
         <h2>Bienvenido al carrito!!!</h2>
+        <div>
+            {cart.map(item => {
+                return <li>{item}</li>;
+            })};
+            <button onClick={clean}>Vaciar el carrito</button>
+            <button onClick={remove}>Vaciar el carrito</button>
+        </div>
+        </>        
     )
 
 };
