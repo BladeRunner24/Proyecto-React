4dcd875d15b2c8531183c9180702ee45ab40e2aa
Carlos Arturo Mihanovich <carlosmihanovich@hotmail.com>
2020-11-27 19:16:09 -0300

    Update cartContext.js

--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -7,16 +7,23 @@ export const UserCartContext = () => useContext(CartContext);
 export default function CartProvider({children, defaultcart}) {
 
     const [cart, setCart] = useState(defaultcart);
-
-    function add(item) {
-        setCart([...cart, item])
+    
+    
+    function add(item) {  
+        if (item.find(p => p.nombre === item.nombre)) {   
+       setCart([...cart, item]) 
+        }else { console.log('El item ya fue comprado')}         
+             
     };
+    function remove (item) {
+        setCart(item.filter(p => p.nombre !== item.nombre))
+    }
 
     function clean() {
         setCart([]);
     };
 
-    return <CartContext.Provider value = {{cart, add, clean}}>
+    return <CartContext.Provider value = {{cart, add, clean, remove}}>
         {children}
     </CartContext.Provider>
-};
+}; 
