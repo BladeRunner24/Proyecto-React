00a470a8c73f4450873219857c55c7ff3ef1a3f3
Carlos Arturo Mihanovich <carlosmihanovich@hotmail.com>
2020-12-14 10:35:58 -0300

    Update cartContext.js

--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -20,20 +20,30 @@ export default function CartProvider({children, defaultcart}) {
         
 
        if(cart.findIndex(prueba) === -1 ){  
-       setCart([...cart, item])                  
+       setCart([...cart, item]);                      
        }else { 
             function change(){
                 total.numero = total.numero + item.numero;
-            } 
+            }
             change(); 
        }     
     };
 
     function remove (item) {
-        setCart(cart.filter(p => p.id !== item.id));
-        const suma1 = cart.reduce((a, b) => a.numero * a.precio + b.numero * b.precio);
-        setSum(suma1);
-        console.log(cart);
+        if (cart.length === 0){
+            setSum(0)
+        }else if(cart.length === 1) {
+            setCart(cart.filter(p => p.id !== item.id));
+            const e = cart.length - 1;
+            const suma3 = cart[e].precio * cart[e].numero;  
+            setSum(suma3);
+            console.log(suma3);
+        }else {
+            setCart(cart.filter(p => p.id !== item.id));
+            const suma1 = cart.reduce((a, b) => a.numero * a.precio + b.numero * b.precio);
+            setSum(suma1);
+             console.log(cart);
+        };
     };
     
     function clean() {
@@ -42,11 +52,17 @@ export default function CartProvider({children, defaultcart}) {
     };
 
     function totalCompra() {
-        
+        if(cart.length === 0) {
+            setSum(0)
+        }else if(cart.length === 1) {
+        const l = cart.length - 1;
+        const suma2 = cart[l].precio * cart[l].numero;  
+        setSum(suma2);
+        console.log(suma2);        
+       }else {
        const suma = cart.reduce((a, b) => a.numero * a.precio + b.numero * b.precio);
-
        setSum(suma);
-       console.log(suma);          
+       };                
     };
     
 
