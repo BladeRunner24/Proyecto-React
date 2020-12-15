cfa027eeeae1a8ec5296f7f8d867ada94b7c2e5d
Carlos Arturo Mihanovich <carlosmihanovich@hotmail.com>
2020-12-15 20:14:56 -0300

    Update itemDetailContainer.js

--- src/components/itemDetailContainer.js
+++ src/components/itemDetailContainer.js
@@ -1,47 +1,41 @@
 import React, {useEffect, useState} from 'react';
 import ItemDetail from "./itemDetail.js";
 import {useParams} from 'react-router-dom';
+import {getFirestore} from '../firebase';
 
 export default function ItemDetailContainer (props) {
 
     const [ detalleProds, setDetalleProds ] = useState([]);
+    const [ load, setLoad ] = useState(null);
     const {id} = useParams();
-    
 
+    
+    
   useEffect(() => {
-    
-    const list = new Promise((res, rej) => {
-      setTimeout(() => {
-        res([
-          {
-            id: '130b',
-            price: 50000,  
-            title: 'Mountain Bike Lince',
-            description: 'Bicicleta de 6 cambios producida por Lince.',             
-          },
-          {
-            id: '152a',
-            price: 65000,
-            title: 'Mountain Bike Tiger',
-            description: 'Bicicleta de 8 cambios producida por Tiger y hecha enteramente con fibra de carbono',              
-          }]
-          );
-      }, 3000);
-    });
-    list.then(listado => {
-      setDetalleProds(listado.filter(l => l.id === id)[0]);      
+    setLoad(true);
+    const db1 = getFirestore();
+    const itemCollection1 = db1.collection("items1");
+    const itemDoc = itemCollection1.where('id', '==', id);
+
+    itemDoc.get().then((querySnapshot) => {
+    if(querySnapshot.size === 0) {
+    console.log('Sin resultados');
+      };
+      setDetalleProds(
+        querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data()}))[0]
+      ); 
+          
        
-    }, err => {
-       console.log(null);
-    }).catch (error => {
-      console.log(`Se ha producido un error ${error}`);
-    })
-  }, [id]);
+      setLoad(false);
+      });  
 
-  
-  
+  },[id]);
 
-    return  (
+   console.log(detalleProds)
+
+    
+
+  return (
         <ItemDetail ids = {detalleProds.id} titulo = {detalleProds.title} descripcion = {detalleProds.description} precio = {detalleProds.price}/>
     )
 };
